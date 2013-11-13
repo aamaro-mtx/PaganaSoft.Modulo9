9407c51f5e53da84fa064e6f87953a427f0577bc
Abel Amaro Julian <amarotux@gmail.com>
2013-11-12 19:44:32 -0600

    Add repository, entities,model, dbcontext and and implement the IRepository interface methods.

---
+++ NorthWindData/Data/NorthWindRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NorthWindData.Entities;
+
+namespace NorthWindData.Data
+{
+    public class NorthWindRepository : IRepository
+    {
+        public NorthwindContext Context { get; private set; }
+        public NorthWindRepository()
+        {
+            Context = new NorthwindContext();
+        }
+
+        public IEnumerable<Category> GetCategories(int catid)
+        {
+            return Context.Categories.Where(c => c.CategoryID == catid);
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(int catid)
+        {
+            return Context.Products.Where(p => p.CategoryID == catid);
+        }
+
+        public IEnumerable<Order_Detail> GetOrdersByProduct(int proid)
+        {
+            return Context.Order_Details.Where(o => o.ProductID == proid);
+        }
+
+        public int GetQuantityByOrder(int ordnum, int proid)
+        {
+            var orderdetail = Context.Order_Details.FirstOrDefault(od => od.ProductID == proid && od.OrderID == ordnum);
+            if (orderdetail != null)
+            {
+                return orderdetail.Quantity;
+            }
+            return -1;
+        }
+
+        public void Dispose()
+        {
+            if (Context != null)
+            {
+                Context.Dispose();
+            }
+        }
+    }
+}
---
+++ NorthWindData/Entities/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NorthWindData.Entities
+{
+    interface IRepository : IDisposable
+    {
+        IEnumerable<Category> GetCategories(int catid);
+        IEnumerable<Product> GetProductsByCategory(int catid);
+        IEnumerable<Order_Detail> GetOrdersByProduct(int proid);
+        int GetQuantityByOrder(int ordnum, int proid);
+    }
+}
